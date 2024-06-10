11c7ab9650c285a6cb21cc0c2b3911fc6fd91f2d
woo5715 <too5715@naver.com>
2024-06-10 16:51:28 +0900

    first commit

---
+++ src/main/java/com/example/jenkins/Controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.jenkins.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String main(){
+        return "hi!";
+    }
+
+    @GetMapping("/a")
+    public String a(){
+        return "a!";
+    }
+
+}
